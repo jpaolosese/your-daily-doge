7a0952f459cefc6e10e2b7eb1a8d9181f2e1cdfd
Paolo Sese <jpaolo.sese@gmail.com>
2016-07-25 12:32:12 -0700

    created html endpoint and port listen in server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-//require express
+// require express
 var express = require('express');
 var app = express();
+
+
+// HTML endpoint
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/*
+*  Server
+*/
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
