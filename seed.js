e150d404c6731e8d2a7c9248f53e8128f2ae29a8
Paolo Sese <jpaolo.sese@gmail.com>
2016-07-26 08:12:06 -0700

    added theme-appropriate style

--- seed.js
+++ seed.js
@@ -4,17 +4,17 @@ var shibaFacts = [
   {
     factIndex: 1,
     text: "The Duke of Venice, known as the Doge di Venezia, was at times an actual Shiba Inu.",
-    image: "http://i.imgur.com/tXWEmL8.jpg"
+    image: "https://i.imgur.com/tXWEmL8.jpg"
   },
   {
     factIndex: 2,
     text: "The Shiba Inu arrived on Earth in the 4th century CE.",
-    image: "http://i.imgur.com/wp9Qr8V.gif"
+    image: "https://i.imgur.com/wp9Qr8V.gif"
   },
   {
     factIndex: 3,
     text: "Shiba Inu is Japanese for brushwood dog.",
-    image: "http://66.media.tumblr.com/tumblr_lqg2t1NYuK1qf9be8o1_500.jpg"
+    image: "https://66.media.tumblr.com/tumblr_lqg2t1NYuK1qf9be8o1_500.jpg"
   },
   {
     factIndex: 4,
@@ -24,42 +24,42 @@ var shibaFacts = [
   {
     factIndex: 5,
     text: "The anime Shiba Inuko-san is about a 14 year old girl who just happens to look like an upright walking Shiba Inu.",
-    image: "http://i.imgur.com/puLhV3Y.gif"
+    image: "https://i.imgur.com/puLhV3Y.gif"
   },
   {
     factIndex: 6,
     text: "The Shiba Inu can, at times, be a total piece of shit.",
-    image: "http://i.imgur.com/mlGlJbq.gif"
+    image: "https://i.imgur.com/mlGlJbq.gif"
   },
   {
     factIndex: 7,
     text: "Bodhi is a Shiba clothing model from New York.",
-    image: "http://i.imgur.com/QZPkMCU.jpg"
+    image: "https://i.imgur.com/QZPkMCU.jpg"
   },
   {
     factIndex: 8,
     text: "Shibas are virtually indistinguishable from marshmallows.",
-    image: "http://i.imgur.com/MWuRMm6.png"
+    image: "https://i.imgur.com/MWuRMm6.png"
   },
   {
     factIndex: 9,
     text: "Shibas grow well in sandy soils.",
-    image: "http://i.imgur.com/r4hCn41.jpg"
+    image: "https://i.imgur.com/r4hCn41.jpg"
   },
   {
     factIndex: 10,
     text: "Brought to you by The Doge Times.",
-    image: "http://i.imgur.com/uYz0AvW.jpg"
+    image: "https://i.imgur.com/uYz0AvW.jpg"
   },
   {
     factIndex: 11,
     text: "Shibas can be total assholes sometimes.",
-    image: "http://i.imgur.com/C1U1yRj.jpg"
+    image: "https://i.imgur.com/C1U1yRj.jpg"
   },
   {
     factIndex: 12,
     text: "What Shibas look like when they ripen.",
-    image: "http://i.imgur.com/uP18cDm.jpg"
+    image: "https://i.imgur.com/uP18cDm.jpg"
   }
 ];
 
