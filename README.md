dfe14a40e4854a3daa581c3da63d487f816787d8
Paolo Sese <jpaolo.sese@gmail.com>
2016-07-26 09:08:08 -0700

    fleshed out README.md

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# your-daily-doge
+# your-daily-doge# App Name
+
+Your Daily Doge! is a random facetious Doge fact generator.
+
+[Link to project hosted on Heroku](https://whispering-atoll-22748.herokuapp.com/)
+
+## Technologies Used
+
+*What technologies did you use to develop this project? (bullet points)*
+This project uses:
++ Express
++ Node.js
++ Mongoose
++ Bootstrap
++ Handlebars
+
+
+## Existing Features
+
+This project features a GET method for a single Doge fact.
+
+
+## Planned Features
+
+*What changes would you make to your project if you continue to work on it? (bullet points)*
+Seeing as this was done in the space of 24 hours, if I were to refactor this project I would:
++ Create more RESTful routes with CRUD
++ Create a comments section so users can comment on facts
++ Create Auth so registered users can have unique personalized usernames
++ Eventually, make this a quasi-wiki style fact compendium.
+
+## Preview
+![Screenshot](/public/screenshot.PNG)
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
-  console.log("app.js loaded and ready!");
 
   // gets single shibe fact from database
   $.get('/api/facts').success(function (shibeFacts) {
     displayFact(shibeFacts[Math.round(Math.random() * (shibeFacts.length - 1))]);
   });
 
-  $("button").click(function handleClick(event) {
-    event.preventDefault();
-    console.log("wow such click " + event);
-    $.get('/api/facts').success(function (shibeFacts) {
-      displayFact(shibeFacts[Math.round(Math.random() * (shibeFacts.length - 1))]);
-    });
-  });
+  $("button").click(handleClick);
 });
 
 function displayFact(fact) {
@@ -20,4 +13,20 @@ function displayFact(fact) {
   var dogeFactTemplate = Handlebars.compile(dogeFactHtml);
   var html = dogeFactTemplate(fact);
   $("#dogefact").html(html);
+}
+
+function replaceFact(fact) {
+  var dogeFactHtml = $("#dogefact-template").text($("#dogefact-template").html());
+  var dogeFactTemplate = Handlebars.compile(dogeFactHtml);
+  var html = dogeFactTemplate(fact);
+  $("#dogefact").empty();
+  $("#dogefact").html(html);
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  console.log("wow such click " + event);
+  $.get('/api/facts').success(function (shibeFacts) {
+    replaceFact(JSON.stringify(shibeFacts[Math.round(Math.random() * (shibeFacts.length - 1))]));
+  });
 }
